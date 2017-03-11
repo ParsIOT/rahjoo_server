a79be17b2df08a5f24f88112c26fcc5011dba062
amir <a.m.salehoof@gmail.com>
2017-03-11 18:21:02 +0330

    Change db

--- boothManagement/admin.py
+++ boothManagement/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Booth_Details)
 admin.site.register(Product_Details)
+admin.site.register(Booth_Owner_Profile)
--- boothManagement/models.py
+++ boothManagement/models.py
@@ -1,12 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Booth_Details(models.Model):
+class Booth_Owner_Profile(models.Model):
 	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=100)
-	owner = models.CharField(max_length=200)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	company = models.CharField(max_length=50)
+	phone = models.CharField(max_length=12)
 	description = models.CharField(max_length=300)
 
 	def __str__(self):
@@ -20,4 +23,7 @@ class Product_Details(models.Model):
 	description = models.CharField(max_length=300)
 	price = models.IntegerField()
 	status = models.BooleanField(default=True)
-	owner_booth = models.ForeignKey(Booth_Details, on_delete=models.CASCADE)
+	owner_booth = models.ForeignKey(Booth_Owner_Profile, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name + " owned by " + self.owner_booth.name
